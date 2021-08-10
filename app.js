b989ab25edb68941e1ed79c355ba14a9903c5edf
BHAVIT-SINGLA <bhavithisar2001@gmail.com>
2021-08-10 14:27:19 +0530

    Script added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const path =require('path'); //Library which have many functions realted to path
 const hbs=require('hbs'); //It uses a template and an input object to generate HTML or other text formats. Handlebars templates look like regular text with embedded Handlebars expressions
-const port=8000;
+const port=process.env.PORT || 8000;;
 app.set('view engine','hbs');// in order to tell express that the view engine is hbs.Viewengine is the combination of HTML tags, Server Controls and a programming language and works inside the application for rendering view to the browser or to the user.
 app.set('views', path.join(__dirname,'/templates/views'));//In order to tell express where view directory is located
 hbs.registerPartials(path.join(__dirname,'/templates/partials'));//Handlebars allows for template reuse through partials. Partials are normal Handlebars templates that may be called directly by other templates.In order to use a partial, it must be registered via Handlebars.registerPartial
